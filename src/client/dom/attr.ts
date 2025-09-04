d5d5945f9fb153fccaf3e3c04d29b520206475d4
velzie <velzie@velzie.rip>
2025-09-04 13:46:55 -0400

    [core] boxing: harden attr.ts

--- src/client/dom/attr.ts
+++ src/client/dom/attr.ts
@@ -1,15 +1,30 @@
-import { ScramjetClient } from "@client/index";
+import {
+	Array_filter,
+	Array_length,
+	Object_keys,
+	Reflect_apply,
+	Reflect_get,
+	Reflect_has,
+	Reflect_ownKeys,
+	ScramjetClient,
+	String_startsWith,
+} from "@client/index";
 
 export default function (client: ScramjetClient, _self: typeof window) {
+	const namedNodeMapProps = Object.getOwnPropertyNames(NamedNodeMap.prototype);
+
+	const Global_isNaN = isNaN;
+	const Global_Number = Number;
+
 	client.Trap("Element.prototype.attributes", {
 		get(ctx) {
 			const map = ctx.get() as NamedNodeMap;
-			const proxy = new Proxy(map, {
+			const proxy = client.natives.construct("Proxy", map, {
 				get(target, prop, _receiver) {
-					const value = Reflect.get(target, prop);
+					const value = Reflect_get(target, prop);
 
 					if (prop === "length") {
-						return Object.keys(proxy).length;
+						return Array_length(Object_keys(proxy));
 					}
 
 					if (prop === "getNamedItem") {
@@ -20,44 +35,49 @@ export default function (client: ScramjetClient, _self: typeof window) {
 							proxy[`${namespace}:${name}`];
 					}
 
-					if (prop in NamedNodeMap.prototype && typeof value === "function") {
+					if (prop in namedNodeMapProps && typeof value === "function") {
 						return new Proxy(value, {
 							apply(target, that, args) {
 								if (that === proxy) {
-									return Reflect.apply(target, map, args);
+									return Reflect_apply(target, map, args);
 								}
 
-								return Reflect.apply(target, that, args);
+								return Reflect_apply(target, that, args);
 							},
 						});
 					}
 
 					if (
 						(typeof prop === "string" || typeof prop === "number") &&
-						!isNaN(Number(prop))
+						!Global_isNaN(Global_Number(prop))
 					) {
-						const position = Object.keys(proxy)[prop];
+						const position = Object_keys(proxy)[prop];
 
 						return map[position];
 					}
 
-					if (!this.has(target, prop)) return undefined;
+					if (!client.natives.call("NamedNodeMap.prototype.has", target, prop))
+						return undefined;
 
 					return value;
 				},
 				ownKeys(target) {
-					const keys = Reflect.ownKeys(target);
+					const keys = Reflect_ownKeys(target);
 
-					return keys.filter((key) => this.has(target, key));
+					return Array_filter(keys, (key) => this.has(target, key));
 				},
 				has(target, prop) {
-					if (typeof prop === "symbol") return Reflect.has(target, prop);
-					if (prop.startsWith("scramjet-attr-")) return false;
-					if (map[prop]?.name?.startsWith("scramjet-attr-")) return false;
+					if (typeof prop === "symbol") return Reflect_has(target, prop);
+					if (String_startsWith(prop, "scramjet-attr-")) return false;
+					if (
+						map[prop]?.name &&
+						String_startsWith(map[prop].name, "scramjet-attr-")
+					)
+						return false;
 
-					return Reflect.has(target, prop);
+					return Reflect_has(target, prop);
 				},
-			});
+			} as ProxyHandler<NamedNodeMap>);
 
 			return proxy;
 		},
@@ -65,15 +85,25 @@ export default function (client: ScramjetClient, _self: typeof window) {
 
 	client.Trap(["Attr.prototype.value", "Attr.prototype.nodeValue"], {
 		get(ctx) {
-			if (ctx.this?.ownerElement) {
-				return ctx.this.ownerElement.getAttribute(ctx.this.name);
+			const ownerElement = client.descriptors.get(
+				"Attr.prototype.ownerElement",
+				ctx.this
+			);
+			if (ownerElement) {
+				// specifically use the proxied version here
+				// TODO less jank
+				return ownerElement.getAttribute(ctx.this.name);
 			}
 
 			return ctx.get();
 		},
 		set(ctx, value) {
-			if (ctx.this?.ownerElement) {
-				return ctx.this.ownerElement.setAttribute(ctx.this.name, value);
+			const ownerElement = client.descriptors.get(
+				"Attr.prototype.ownerElement",
+				ctx.this
+			);
+			if (ownerElement) {
+				return ownerElement.setAttribute(ctx.this.name, value);
 			}
 
 			return ctx.set(value);
--- src/client/helpers.ts
+++ src/client/helpers.ts
@@ -3,3 +3,34 @@ export function getOwnPropertyDescriptorHandler(target, prop) {
 
 	return realDescriptor;
 }
+
+export const Object_keys = Object.keys;
+export const Object_values = Object.values;
+export const Object_getOwnPropertyDescriptor = Object.getOwnPropertyDescriptor;
+
+export const Reflect_get = Reflect.get;
+export const Reflect_ownKeys = Reflect.ownKeys;
+export const Reflect_has = Reflect.has;
+export const Reflect_apply = Reflect.apply;
+
+export const Global_Number = Number;
+export const Global_isNaN = isNaN;
+
+const array_length_desc = Object_getOwnPropertyDescriptor(
+	Array.prototype,
+	"length"
+).get;
+export const Array_length = (array: Array<any>) => {
+	Reflect_apply(array_length_desc, array, []);
+};
+
+// safe to be in here - ArraySpeciesCreate() will construct the array from the original realm
+const array_filter_func = Array.prototype.filter;
+export function Array_filter<T>(array: Array<T>, func: (arg: T) => boolean) {
+	return Reflect_apply(array_filter_func, array, [func]);
+}
+
+const string_startsWith_func = String.prototype.startsWith;
+export function String_startsWith(str: string, search: string) {
+	return Reflect_apply(string_startsWith_func, str, [search]);
+}
