96587c4f7db68719c0f049ffeb24ee217160b68c
velzie <velzie@velzie.rip>
2025-10-08 20:38:15 -0400

    [chrome] allow singlefile builds with vite-plugin-singlefile

--- packages/chrome/vite.config.ts
+++ packages/chrome/vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import { scramjetPath } from "@mercuryworkshop/scramjet/path";
 
+import { viteSingleFile } from "vite-plugin-singlefile";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
 	plugins: [
+		process.env.VITE_SINGLEFILE ? viteSingleFile() : null,
 		viteStaticCopy({
 			structured: false,
 			targets: [
